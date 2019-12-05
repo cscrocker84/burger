776ae2f1c5a244208deaca28c95a194d5fcb1245
Charlie Crocker <cscrocker84@gmail.com>
2019-12-05 14:36:40 -0600

    First commit, building the structure

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers(burger_name, devoured) VALUES("Cheese Burger", false);
+INSERT INTO burgers(burger_name, devoured) VALUES("Bacon Bison Burger", true);
+INSERT INTO burgers(burger_name, devoured) VALUES("Fois Gras and Kobe Beef Burger", false);
