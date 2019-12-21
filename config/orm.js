0475ba13ae56e9c3fe3b6e650f7bf7b4ce2a0098
Katherine Crocker <katherinecrocker@Katherines-MacBook-Air-2.local>
2019-12-21 11:40:59 -0600

    Working on the config

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,56 @@
-var connection = require("../config/connection.js");
+const db = require('./connection')
+const mysql = require('mysql')
 
+const orm = {
+    // Helper function to convert object key/value pairs to SQL syntax
+    objToSql: (ob) => {
+        var arr = [];
 
+        // loop through the keys and push the key/value as a string int arr
+        for (var key in ob) {
+            var value = ob[key];
+            // check to skip hidden properties
+            if (Object.hasOwnProperty.call(ob, key)) {
+                // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+                if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                    value = "'" + value + "'";
+                }
+                // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+                // e.g. {sleepy: true} => ["sleepy=true"]
+                arr.push(key + "=" + value);
+            }
+        }
 
+        // translate array of strings to a single comma-separated string
+        return arr.toString();
+    },
 
+    selectAll: (cb) => {
+        const query = 'SELECT * FROM burgers'
+        db.query(query, cb)
+    },
 
+    insertOne: (name, cb) => {
+        const query = 'INSERT INTO burgers (burger_name) VALUES (?)'
+        db.query(query, [name], cb)
+    },
 
+    updateOne: (set, where, cb) => {
+        let query = 'UPDATE burgers SET '
+        query += orm.objToSql(set)
+        query += ' WHERE '
+        query += orm.objToSql(where)
 
+        // query = mysql.format(query, [set, where])
 
+        const q = db.query(query, [set, where], cb)
+    },
 
+    deleteOne: (id, cb) => {
+        let query = 'DELETE FROM burgers WHERE `id` = ?'
 
+        db.query(query, [id], cb)
+    }
+}
 
-module.exports = orm;
+module.exports = orm
--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 var express = require("express");
 
 var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// serve static content for the app from the public directory
+app.use(express.static("public"));
+
+// parse body requests as JSON
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// set handlebars
+var exhbs = require("express-handlebars");
+
+// middleware for handlebars
+app.engine("handlebars", exhbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// import routes and give server acces
+var routes = require("./controllers/burgers_controller");
+
+app.use(routes);
+
+// listen for requests
+app.listen(PORT, function(){
+    console.log("Listening on PORT: " + PORT)
+})
