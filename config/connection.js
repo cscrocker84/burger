20dc47856eccec866fdcdf36d1b48f5c743421df
Charlie Crocker <cscrocker84@gmail.com>
2020-01-09 15:23:09 -0600

    fixing some bugs

--- config/connection.js
+++ config/connection.js
@@ -9,6 +9,7 @@ if (process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
         host: "localhost",
+        port: 3306,
         user: "root",
         password: "Ohiosucks11",
         database: "burgers_db"
