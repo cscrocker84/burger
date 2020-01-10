44e8513149f1d49314437790bf4e58b1f8b6c644
Charlie Crocker <cscrocker84@gmail.com>
2020-01-09 19:36:41 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -16,7 +16,7 @@ if (process.env.JAWSDB_URL) {
     });
 };
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
       console.error("error connecting: " + err.stack);
       return;
